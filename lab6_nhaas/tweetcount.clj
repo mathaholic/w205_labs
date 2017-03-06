46f7f9b59646814799b8420bd8f75613b6baa146
Nikki <nikkilovesmath@gmail.com>
2017-03-05 16:20:30 -0800

    lab 6 commit

---
+++ lab6_nhaas/tweetcount.clj
@@ -0,0 +1,38 @@
+(ns tweetcount
+    (:use     [streamparse.specs])
+    (:gen-class))
+
+(defn tweetcount [options]
+  [
+    ;; spout configuration
+    {"X-spout" (python-spout-spec
+        options
+        "spouts.sentences.Sentences"
+        [sentence]
+        )
+    }
+    ;; Bolts
+    {
+        ;; bolt configuration 1
+        {"ParseTweet" (python-bolt-spec
+          options
+          {"Sentences" :shuffle}
+          "bolts.parse.ParseTweet"
+          ["valid_words"]
+          ;; for two bolts
+          :p 2
+          )
+        }
+        ;; bolt configuration 2
+        {"TweetCounter" (python-bolt-spec
+          options
+          {"Sentences" :shuffle}
+          "bolts.tweetcounter.TweetCounter"
+          ["word", "count"]
+          :p 1
+          ;; only need 1 bolt
+          )
+        }
+    }
+  ]
+)
